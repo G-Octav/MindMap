0108b0bfb2627f84c9b17c8acc9aa403437132ae
Gensthaler Octavian <octav.gensthaler@gmail.com>
2014-07-13 21:02:39 -0700

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MindMap.Startup))]
+namespace MindMap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
